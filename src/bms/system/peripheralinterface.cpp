af41e53d317af19d1251887261b12f983669996f
jjyeh1976@gmail.com <jjyeh1976@gmail.com>
2023-05-10 12:16:24 +0800

    read dio and adc character device done

--- src/bms/system/peripheralinterface.cpp
+++ src/bms/system/peripheralinterface.cpp
@@ -6,6 +6,8 @@
 #include <fcntl.h>
 #include <QProcess>
 #include <QString>
+#include <QTextStream>
+#include <QTimer>
 
 PeripheralInterface::PeripheralInterface()
 {
@@ -137,4 +139,155 @@ int IIODevice::readChannel(int channel)
         return _results[channel];
     }
     return -1;
+}
+
+
+/*  gpio handler */
+
+GPIOHandler::GPIOHandler(QObject *parent)
+    :GPIOHandler(parent,1,INPUT)
+{
+
+}
+
+GPIOHandler::GPIOHandler(QObject *parent, int gpioId, GPIODIR dir)
+    :QObject(parent)
+    ,_gpio(gpioId)
+    ,_dir(dir)
+{
+    QString cmdStr = QString("echo %1 > /sys/class/gpio/export").arg(_gpio);
+    system(cmdStr.toUtf8().constData());
+
+    _fd = -1;
+    _file = nullptr;
+    _timer = new QTimer();
+    _lastValue = -1;
+    connect(_timer,&QTimer::timeout,this,&GPIOHandler::handleTimeout);
+    QString path = QString("/sys/class/gpio/gpio%1/").arg(_gpio);
+    if(QFile(path).exists()){
+        // set direction
+        cmdStr = QString("echo %1 >/sys/class/gpio/gpio%2/direction").arg((dir == INPUT)?"in":"out").arg(_gpio);
+        system(cmdStr.toUtf8().constData());
+
+        openDevice();
+        // read initial value
+        readValue();
+
+
+    }
+}
+
+void GPIOHandler::openDevice()
+{
+    _timer->stop();
+    if(_file  == nullptr){
+        QString path = QString("/sys/class/gpio/gpio%1/value").arg(_gpio);
+        _file  = new QFile();
+        _file->setFileName(path);
+        _timer->start(100);
+        //_file->open(QFile::ReadOnly);
+
+
+//        if(_file->exists()){
+//            _file->open(QFile::ReadOnly);
+//            _notifier = new QSocketNotifier(_file->handle(),QSocketNotifier::Read);
+//            connect(_notifier,&QSocketNotifier::activated,this,&GPIOHandler::handleActivate);
+//        }
+    }
+}
+
+int GPIOHandler::readValue()
+{
+    int value = -1;
+    if(_file != nullptr){
+        _file->open(QFile::ReadOnly);
+        QTextStream ts(_file);
+        value = ts.readLine().toInt();
+        _file->close();
+        if(_lastValue != value){
+            _lastValue = value;
+            emit gpioChanged(_gpio,_lastValue);
+        }
+    }
+    return value;
+}
+
+void GPIOHandler::handleActivate(int socket)
+{
+    readValue();
+}
+
+void GPIOHandler::handleTimeout()
+{
+    readValue();
+}
+
+
+/* adc handler */
+
+ADCHandler::ADCHandler(QObject *parent)
+    :ADCHandler("",0,parent)
+{
+
+}
+
+ADCHandler::ADCHandler(QString devName,int channel, QObject *parent)
+    :_devName(devName)
+    ,_ch(channel)
+{
+    _offset = 0;
+    _scale = 1.0;
+    _timer = new QTimer();
+    connect(_timer,&QTimer::timeout,this,&ADCHandler::handleTimeout);
+    openDevice();
+}
+
+void ADCHandler::openDevice()
+{
+    QString cmdStr = QString("/sys/bus/iio/devices/%1/in_voltage%2_raw").arg(_devName).arg(_ch);
+    _file = new QFile(cmdStr);
+    if(_file->exists()){
+        _timer->start(100);
+    }
+    else{
+        delete _file;
+        _file = nullptr;
+    }
+
+}
+
+void ADCHandler::setScale(float scale)
+{
+    _scale = scale;
+}
+
+void ADCHandler::setOffset(int offset)
+{
+    _offset = offset;
+}
+
+
+
+void ADCHandler::handleTimeout()
+{
+    readValue();
+}
+
+int ADCHandler::readValue()
+{
+    int value = -1;
+    if(_file != nullptr){
+        _file->open(QFile::ReadOnly);
+        QTextStream ts(_file);
+        value = ts.readLine().toInt();
+        _file->close();
+//        value -= _offset;
+//        _scaledValue = value * _scale;
+        emit updateValue(_ch,value);
+    }
+}
+
+float ADCHandler::scaledValue()
+{
+    return _scaledValue;
 }
